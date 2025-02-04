bef242f80b9dce2e6e3796dc7ea906028c0eb32f
Anthony <anthony.harley32@gmail.com>
2025-02-03 20:10:47 -0600

    Initial Commit

---
+++ VistaVid/VistaVidApp.swift
@@ -0,0 +1,17 @@
+//
+//  VistaVidApp.swift
+//  VistaVid
+//
+//  Created by anthony on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct VistaVidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
